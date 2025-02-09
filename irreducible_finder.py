2e06665e85de12eeb2255e2dc88f61c1a2665ef8
Daniel Lewis <danielqlewis.dur@gmail.com>
2025-02-08 20:10:56 -0700

    Update irreducible_finder.py
    Added function 'compute_large_exponent_of_x'. Updated functions 'check_if_irreducible' and 'find_irreducible'. This is now a working version of this module.

--- irreducible_finder.py
+++ irreducible_finder.py
@@ -1,5 +1,19 @@
 from modular_polynomial import ModularPolynomial
 
+PRIME_FACTORS = {
+    1: [1],
+    2: [2],
+    3: [3],
+    4: [2],
+    5: [5],
+    6: [2, 3],
+    7: [7],
+    8: [2],
+    9: [3],
+    10: [2, 5],
+    11: [11]
+}
+
 
 def check_if_primitive(num, prime_modulus):
     order = 1
@@ -9,6 +23,35 @@ def check_if_primitive(num, prime_modulus):
         order += 1
 
     return order == prime_modulus - 1
+
+
+def compute_large_exponent_of_x(target_power, modulus_polynomial):
+    p = modulus_polynomial.modulus
+    d = modulus_polynomial.get_degree()
+    power = 1
+    active_expression = ModularPolynomial(p, [0, 1])
+    expression_record = [(active_expression.get_copy(), power)]
+
+    while power < target_power:
+        add_expression_to_record = False
+        if power * 2 > target_power:
+            for x in reversed(expression_record):
+                if x[1] <= target_power - power:
+                    active_expression = active_expression.product_with(x[0])
+                    power += x[1]
+                    break
+        else:
+            active_expression = active_expression.product_with(active_expression)
+            power *= 2
+            add_expression_to_record = True
+
+        if active_expression.get_degree() >= d:
+            active_expression = active_expression.divided_by(modulus_polynomial).remainder
+
+        if add_expression_to_record:
+            expression_record.append((active_expression.get_copy(), power))
+
+    return active_expression
 
 
 def check_if_irreducible(poly):
@@ -27,8 +70,19 @@ def check_if_irreducible(poly):
                 return False
         return True
 
-    # Higher degrees will need a different approach
-    raise NotImplementedError("Irreducibility test not implemented for degree >= 4")
+    # Higher degree cases: use irreducibility criterion
+    standard_poly = ModularPolynomial(poly.modulus, [0, 1])
+
+    power = poly.modulus ** poly.get_degree()
+    first_check = compute_large_exponent_of_x(power, poly)
+    if first_check != standard_poly:
+        return False
+    for r in PRIME_FACTORS[poly.get_degree()]:
+        power = poly.modulus ** (poly.get_degree() // r)
+        active_check = compute_large_exponent_of_x(power, poly)
+        if active_check == standard_poly:
+            return False
+    return True
 
 
 def find_irreducible(p, n):
@@ -41,12 +95,18 @@ def find_irreducible(p, n):
         return ModularPolynomial(p, [primitive_elements[0], leading_term])
 
     else:
-        for constant in primitive_elements:
-            for y in range(1, p):
-                for x in range(1, n - 1):
-                    lower_term_zeros = [0] * (x - 1)
-                    higher_term_zeros = [0] * (n - x - 2)
-                    coefficient_set = [constant] + lower_term_zeros + [y] + higher_term_zeros + [leading_term]
-                    polynomial = ModularPolynomial(p, coefficient_set)
-                    if check_if_irreducible(polynomial):
-                        return polynomial
+        if (p == 2 and n == 9) or (p == 3 and n == 11):
+            if p == 2:
+                return ModularPolynomial(2, [1, 1, 0, 0, 0, 0, 1, 1, 1])
+            elif p == 3:
+                return ModularPolynomial(3, [2, 0, 2, 0, 1, 0, 0, 0, 0, 2, 1])
+        else:
+            for constant in primitive_elements:
+                for y in range(1, p):
+                    for x in range(1, n - 1):
+                        lower_term_zeros = [0] * (x - 1)
+                        higher_term_zeros = [0] * (n - x - 2)
+                        coefficient_set = [constant] + lower_term_zeros + [y] + higher_term_zeros + [leading_term]
+                        polynomial = ModularPolynomial(p, coefficient_set)
+                        if check_if_irreducible(polynomial):
+                            return polynomial

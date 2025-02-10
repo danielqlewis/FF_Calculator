1052eb15fa054894255172ed3d907e238ea2ac2a
Daniel Lewis <danielqlewis.dur@gmail.com>
2025-02-09 21:15:42 -0700

    Update calculator_engine.py
    Started process of updating 'divides' case to correctly multiply my multiplicative inverse

--- calculator_engine.py
+++ calculator_engine.py
@@ -5,6 +5,9 @@ class FiniteFieldCalculator:
         self.prime_modulus = prime_modulus
         self.polynomial_modulus = polynomial_modulus
 
+    def find_multiplicative_inverse(self, polynomial):
+        """Find multiplicative inverse of polynomial in the field"""
+        raise NotImplementedError()
 
     def handle_operation(self, coefficient_set_0, coefficient_set_1, op):
         polynomial0 = ModularPolynomial(self.prime_modulus, coefficient_set_0)
@@ -15,7 +18,9 @@ class FiniteFieldCalculator:
         elif op == "subtract":
             return polynomial1.subtract_from(polynomial0)
         elif op == "divide":
-            return polynomial0.divided_by(polynomial1).quotient
+            polynomial1_inverse = self.find_multiplicative_inverse(polynomial1)
+            product = polynomial0.product_with(polynomial1_inverse)
+            return product.divided_by(self.polynomial_modulus).remainder
         elif op == "multiply":
-            initial_expression = polynomial0.product_with(polynomial1)
-            return initial_expression.divided_by(self.polynomial_modulus).remainder
+            product = polynomial0.product_with(polynomial1)
+            return product.divided_by(self.polynomial_modulus).remainder

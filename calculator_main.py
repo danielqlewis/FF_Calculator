9f5ab1e518dbeee76578a895ba5ec222455244fe
Daniel <omega0800@gmail.com>
2025-03-03 14:20:15 -0700

    add module docstring

--- calculator_main.py
+++ calculator_main.py
@@ -1,3 +1,14 @@
+"""
+Finite Field Calculator - Main Entry Point
+
+This module serves as the entry point for the Finite Field Calculator application,
+which allows users to perform arithmetic operations in finite fields of the form GF(p^n).
+
+Author: Daniel Lewis
+Version: 1.0 (March 2025)
+GitHub: https://github.com/danielqlewis/FF_Calculator
+"""
+
 from gui_coordinator import GuiCoordinator
 from calculator_controller import CalculatorController
 
